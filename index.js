e00fadda8da92710efe0f4f6ab98403e9581dd5c
Megan Sullivan <meganesulli@gmail.com>
2017-02-11 15:43:38 -0500

    Add friend form

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+app.get('/addfriend', function(request, response) {
+  response.render('pages/addFriend');
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
